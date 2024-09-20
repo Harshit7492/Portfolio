1c1585d30a3bc61d4d07c6e34c5ecd623e43b05a
Harshit <harshit28k@gmail.com>
2024-09-20 23:51:01 +0530

    Portfolio new changes

--- src/database/index.js
+++ src/database/index.js
@@ -1,17 +1,19 @@
 // const express=require('express')
 
 const mongoose=require('mongoose')
+require('dotenv').config();
 
+const URI = process.env.MONGODB_URI;
 
 export default async function conectToDB(params) {
     try {
-        await mongoose.connect('mongodb+srv://Harshit:xpFznSBcUQ2yibap@cluster0.xh1qn.mongodb.net/',{
+        await mongoose.connect(URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
-
+            
         })
-
         console.log("Database is connected succesfully")
+
     } catch (err) {
         console.log(err)
         
